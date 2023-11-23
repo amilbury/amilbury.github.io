0abb44546a4b740b1d17afc7f9528058ef9aa69e
Aaronias4 <111252509+Aaronias4@users.noreply.github.com>
2023-11-22 21:13:19 -0400

    built tabs

---
+++ main.js
@@ -0,0 +1,35 @@
+window.onload = function(){
+    document.querySelector("#about").addEventListener("click", abtMe);
+    document.querySelector("#project").addEventListener("click", projects);
+    document.querySelector("#experience").addEventListener("click", experience);
+
+}
+
+
+function abtMe(){
+    hideAll();
+    let btn = document.querySelector("#abt");
+    btn.classList.remove("hidden");
+}
+function projects(){
+    hideAll();
+    let btn = document.querySelector("#proj");
+    console.log(btn)
+    btn.classList.remove("hidden");    
+}
+function experience(){
+    hideAll();
+    let btn = document.querySelector("#exp");
+    btn.classList.remove("hidden");    
+}
+
+
+function hideAll(){
+    let abt = document.querySelector("#abt");
+    abt.classList.add("hidden");
+    let proj = document.querySelector("#proj");
+    proj.classList.add("hidden");
+    let exp = document.querySelector("#exp");
+    exp.classList.add("hidden");
+
+}
